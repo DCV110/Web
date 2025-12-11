83b377c85d85d35f7d03d187ac1ca0ec4a7580ec
DCV110 <daicamvinh4862@gmail.com>
2025-12-11 18:06:57 +0700

    lap 5

---
+++ TodoApi/Model/TodoItemDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoApi.Model
+{
+    public class TodoItemDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public TodoItemDTO() { }
+        public TodoItemDTO(Todo todoItem)
+        {
+            Id = todoItem.Id;
+            Name = todoItem.Name;
+            IsComplete = todoItem.IsComplete;
+        }
+    }
+}
